352039ec9caa7bbb6571f4a0ae8bff8f60b0ef66
Breno Candido Conrado - External <e-candido@weg.net>
2022-04-22 15:25:35 -0300

    QOL fixes

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@ static void printVector(SvmVector_t vec);
 int main()
 {
     FILE *fp_smv_data;
+    fp_smv_data = fopen("waveforms.smv", "w+");
+    fprintf(fp_smv_data, "Time,M,Sector,Theta,Alpha,Beta,Theta_mod,T1,T2,T0,gU1,gU2,gV1,gV2,gW1,gW2,cycleTime,Sequence,Vector\n");
 
     float t = 0.0f;
     float elapsed_time = 0.0f;
@@ -25,8 +27,14 @@ int main()
 
     svm.m = 1.0f;
     svm.freq = 60.0f;
-    svm.deltaT = TS;
     svm.ts = TS;
+
+    // Executes once to start at a valid value
+    // deltaT is initialized as zero, so theta won't be incremented
+    executeSVM(&svm);
+
+    // Sets deltaT after initialization
+    svm.deltaT = TS;
 
     gateSeq.ts = TS;
     gateSeq.seqType = SEQ_AB;
@@ -34,8 +42,6 @@ int main()
     // Auxiliary for printing when vector changes
     SvmVector_t prevVec = gateSeq.currentVector;
 
-    fp_smv_data = fopen("waveforms.smv", "w+");
-    fprintf(fp_smv_data, "Time,M,Sector,Theta,Alpha,Beta,Theta_mod,T1,T2,T0,Vector,gU1,gU2,gV1,gV2,gW1,gW2,cycleTime,Sequence\n");
     while (t < SIMULATION_TIME)
     {
         if (elapsed_time >= TS)
@@ -62,10 +68,10 @@ int main()
             printf("\t%s\t%d\n", (gateSeq.currentSeq == SEQ_A)? "SEQ_A" : "SEQ_B", gateSeq.sector);
         }
         
-        fprintf(fp_smv_data, "%f,%f,%d,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d,%f,%d\n", 
+        fprintf(fp_smv_data, "%f,%f,%d,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%f,%d,%d\n", 
                 t, svm.m, svm.sector, svm.theta, svm.alpha, svm.beta, svm.modTheta, svm.t1, svm.t2, svm.t0,
-                gateSeq.currentVectorSelect, gateSeq.gU1, gateSeq.gU2, gateSeq.gV1, gateSeq.gV2, gateSeq.gW1, gateSeq.gW2, 
-                gateSeq.cycleTime,gateSeq.currentSeq);
+                gateSeq.gU1, gateSeq.gU2, gateSeq.gV1, gateSeq.gV2, gateSeq.gW1, gateSeq.gW2, 
+                gateSeq.cycleTime, gateSeq.currentSeq, gateSeq.currentVectorSelect);
 
         t += TIMESTEP;
     }
